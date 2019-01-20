cc0158ea305a2cddbc1b4018a396922d7360dd20
MrZhang <xiugangzhang@uestc.edu.cn>
2019-01-20 18:39:29 +0800

    update test.sql

--- test.sql
+++ test.sql
@@ -1,40 +1,8 @@
-
--- ----------------------------
--- Records of movies
--- ----------------------------
-INSERT INTO `movies` VALUES ('129999', '狄仁杰之通天帝国', 'http://www.iqiyi.com/v_19rrjdhsmc.html', '徐克打造绚丽盛唐', 'http://pic9.iqiyipic.com/image/20180829/1f/69/v_50049666_m_601_m11_180_236.jpg', '<strong class=\"num\">7</strong>.2', '12', '0', '2018-09-03 22:41:42', '2018-09-03 22:41:40', '电影');
-INSERT INTO `movies` VALUES ('130000', '降魔武僧', 'http://www.iqiyi.com/v_19rqzn0c54.html', '佛魔大战一触即发', 'http://pic8.iqiyipic.com/image/20180830/fe/b7/v_118519831_m_601_m3_180_236.jpg', '<strong class=\"num\">6</strong>.2', '19', '0', '2018-09-03 22:41:42', '2018-09-03 22:41:40', '电影');
-INSERT INTO `movies` VALUES ('130001', '天下第一镖局', 'http://www.iqiyi.com/v_19rr1wzek8.html', '樊少皇血战江湖', 'http://pic0.iqiyipic.com/image/20180826/91/0c/v_117128597_m_601_m10_180_236.jpg', '<strong class=\"num\">7</strong>.5', '14', '0', '2018-09-03 22:41:42', '2018-09-03 22:41:40', '电影');
-INSERT INTO `movies` VALUES ('130002', '史密斯夫妇', 'http://www.iqiyi.com/v_19rrgyrhi4.html', '皮特茱莉性感杀手激战', 'http://pic5.iqiyipic.com/image/20180215/da/1c/v_104786994_m_601_m6_180_236.jpg', '<strong class=\"num\">8</strong>.9', '10', '0', '2018-09-03 22:41:42', '2018-09-03 22:41:40', '电影');
-INSERT INTO `movies` VALUES ('130003', '蚁人', 'http://www.iqiyi.com/v_19rrklq2bs.html', '逗比英雄逆袭', 'http://pic1.iqiyipic.com/image/20180822/4d/10/v_109573946_m_601_m6_180_236.jpg', '<strong class=\"num\">8</strong>.8', '14', '0', '2018-09-03 22:41:42', '2018-09-03 22:41:40', '电影');
-INSERT INTO `movies` VALUES ('130008', '关云长', 'http://www.iqiyi.com/v_19rrjaeap4.html', '甄子丹姜文文武对手戏', 'http://pic1.iqiyipic.com/image/20180211/1f/61/v_50101689_m_601_m7_180_236.jpg', '<strong class=\"num\">7</strong>.9', '21', '0', '2018-09-03 22:41:42', '2018-09-03 22:41:40', '电影');
-INSERT INTO `movies` VALUES ('130009', '碟中谍3', 'http://www.iqiyi.com/v_19rrk3pl18.html', '克鲁斯救娇妻重出江湖', 'http://pic9.iqiyipic.com/image/20180211/c2/e3/v_50112692_m_601_m5_180_236.jpg', '<strong class=\"num\">8</strong>.8', '3', '0', '2018-09-03 22:41:42', '2018-09-03 22:41:40', '电影');
-INSERT INTO `movies` VALUES ('130010', '猩球崛起3：终极之战', 'http://www.iqiyi.com/v_19rr7pggvo.html', '打响人猿终极战', 'http://pic2.iqiyipic.com/image/20180220/bf/a9/v_112879834_m_601_m3_180_236.jpg', '<strong class=\"num\">8</strong>.5', '2', '0', '2018-09-03 22:41:42', '2018-09-03 22:41:40', '电影');
-INSERT INTO `movies` VALUES ('130011', '超时空同居', 'http://www.iqiyi.com/v_19rrcd03uk.html', '徐峥扮扯面师傅', 'http://pic6.iqiyipic.com/image/20180704/f0/4e/v_115469489_m_601_m7_180_236.jpg', '<strong class=\"num\">8</strong>.5', '2', '0', '2018-09-03 22:41:42', '2018-09-03 22:41:40', '电影');
-INSERT INTO `movies` VALUES ('130012', '监狱风云', 'http://www.iqiyi.com/v_19rrjzym64.html', '周润发梁家辉疯狂飙戏', 'http://pic8.iqiyipic.com/image/20180212/3f/f3/v_50175540_m_601_m5_180_236.jpg', '<strong class=\"num\">9</strong>.1', '8', '0', '2018-09-03 22:41:42', '2018-09-03 22:41:40', '电影');
-
--- ----------------------------
--- Records of tvs
--- ----------------------------
-INSERT INTO `tvs` VALUES ('1', '等到烟暖雨收第一季', '元气少女蜜恋冷峻公子', 'http://www.iqiyi.com/v_19rqzkkspw.html', '2018-09-11 22:07:38', '电视剧');
-INSERT INTO `tvs` VALUES ('2', '娘道', '岳丽娜谱写母爱传奇', 'http://www.iqiyi.com/v_19rqzc25ao.html', '2018-09-11 22:07:38', '电视剧');
-INSERT INTO `tvs` VALUES ('3', '舌害', '宋祖儿探秘娱乐圈', 'http://www.iqiyi.com/v_19rqzepgjs.html', '2018-09-11 22:07:38', '电视剧');
-INSERT INTO `tvs` VALUES ('4', '为了你我愿意热爱整个世界', '罗晋郑爽相恋二十年', 'http://www.iqiyi.com/v_19rr1bx534.html', '2018-09-11 22:07:38', '电视剧');
-INSERT INTO `tvs` VALUES ('8', '盗墓笔记', '地下惊奇世界探秘之旅', 'http://www.iqiyi.com/v_19rrohr1jc.html', '2018-09-11 22:07:38', '电视剧');
-INSERT INTO `tvs` VALUES ('22', '天泪传奇之凤凰无双', '王丽坤深宫权谋', 'http://www.iqiyi.com/v_19rr8s9o0k.html', '2018-09-11 22:07:38', '电视剧');
-INSERT INTO `tvs` VALUES ('25', '杨光的快乐生活之好好先生', '逗贫杨光再次来袭', 'http://www.iqiyi.com/v_19rqz2p8og.html', '2018-09-11 22:07:38', '电视剧');
-INSERT INTO `tvs` VALUES ('26', '家有儿女初长成VIP独享版', '升级版家有儿女欢乐多', 'http://www.iqiyi.com/v_19rr0n2kjo.html', '2018-09-11 22:07:38', '电视剧');
-INSERT INTO `tvs` VALUES ('27', '灵魂摆渡3', '铁三角身份揭秘', 'http://www.iqiyi.com/v_19rr9evutk.html', '2018-09-11 22:07:38', '电视剧');
-
-
--- ----------------------------
--- Records of preview
--- ----------------------------
-INSERT INTO `preview` VALUES ('1', '狄仁杰之四大天王', '赵又廷破幻术奇案', 'http://www.iqiyi.com/v_19rr7pi4k4.html', '/www/images/big0.jpg');
-INSERT INTO `preview` VALUES ('2', '前任3再见前任', '韩庚郑恺战前任', 'http://www.iqiyi.com/v_19rr7pgf14.html', '/www/images/big1.jpg');
-INSERT INTO `preview` VALUES ('3', '碟中谍神秘国度', '靓汤高空徒手扒飞机', 'http://www.iqiyi.com/v_19rrkq3b00.html', '/www/images/big2.jpg');
-INSERT INTO `preview` VALUES ('4', '降魔武僧', '佛魔大战一触即发', 'http://www.iqiyi.com/v_19rqzn0c54.htm', '/www/images/big3.jpg');
-INSERT INTO `preview` VALUES ('5', '荡寇风云', '赵文卓洪金宝抗倭', 'http://www.iqiyi.com/v_19rr7kt8f4.html', '/www/images/big4.jpg');
-INSERT INTO `preview` VALUES ('6', '家有喜事', '家有喜事删减片段曝光', 'http://www.iqiyi.com/v_19rrlaon50.html', '/www/images/big5.jpg');
-INSERT INTO `preview` VALUES ('7', '唐人街探案2', '王宝强爆笑破案', 'http://www.iqiyi.com/v_19rr839kro.html', '/www/images/big6.jpg');
-INSERT INTO `preview` VALUES ('8', '陈翔六点半铁头无敌', '铁头传人战江湖', 'http://www.iqiyi.com/v_19rr0t840s.html', '/www/images/big7.jpg');
+INSERT INTO `preview` VALUES ('1', '大话西游之大圣娶亲', '我的盖世英雄', 'http://www.iqiyi.com/v_19rrkqj66k.html', '/www/images/big0.jpg');
+INSERT INTO `preview` VALUES ('2', '我不是药神', '30亿票房口碑爆款', 'http://www.iqiyi.com/v_19rreozmq8.html', '/www/images/big1.jpg');
+INSERT INTO `preview` VALUES ('3', '疯狂动物城', '“甄嬛”献声兔朱迪', 'http://www.iqiyi.com/v_19rrlpltbg.html', '/www/images/big2.jpg');
+INSERT INTO `preview` VALUES ('4', '前任3再见前任', '韩庚郑恺战前任', 'http://www.iqiyi.com/v_19rr7pgf14.html', '/www/images/big3.jpg');
+INSERT INTO `preview` VALUES ('5', '碟中谍神秘国度', '靓汤高空徒手扒飞机', 'http://www.iqiyi.com/v_19rrkq3b00.html', '/www/images/big4.jpg');
+INSERT INTO `preview` VALUES ('6', '爱情公寓', '原班人马欢乐重聚', 'http://www.iqiyi.com/v_19rrdeqs18.html', '/www/images/big5.jpg');
+INSERT INTO `preview` VALUES ('7', '后来的我们', '井柏然十年苦恋', 'http://www.iqiyi.com/v_19rrf0jhms.html', '/www/images/big6.jpg');
+INSERT INTO `preview` VALUES ('8', '西虹市首富', '沈腾变身暴富锦鲤', 'http://www.iqiyi.com/v_19rrf2nw1g.html', '/www/images/big7.jpg')
