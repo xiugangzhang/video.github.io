5c7f23b8a6b73ac25ac51e699f6fb6ba819a08e8
zhangxiugang <zhangxiugang@baidudeMacBook-Pro-2.local>
2019-05-05 12:42:35 +0800

    评论页数据分页问题修复

--- controllers/comment.js
+++ controllers/comment.js
@@ -15,66 +15,37 @@ moment.locale('zh-cn');
  */
 exports.showComment = function (req, res, next) {
     // 获取当前显示的页数
-    let pageNow = parseInt(req.params.currentPage);
+    let pageNow = parseInt(req.params.currentPage) || 1;
     let pageSize = req.app.locals.config.pageSize;
     // 每页显示5个
     let params = {
-        // (pageNo-1)*pageSize
         start: (pageNow - 1) * pageSize,
         pageSize: pageSize
     }
-
-    // console.log(params);
-    // console.log(pageSize, pageNow)
-
-    // 获取
-    if (pageNow === 1) {
-        let id = req.session.user.id;
-        Comment.getCommentsById(id, function (err, result) {
+    // 开始查询总的数量信息
+    Comment.getCommentNums(function (err, result) {
+        if (err) {
+            return next(err);
+        }
+        result = result.pageNums || 1;
+        let pageNum = Math.ceil(result / pageSize);
+        // 开始查询数据
+        Comment.getCommentByCurrentPage(params, function (err, result) {
             if (err) {
                 return next(err);
             }
             // 修改时间显示的格式
-            if (result) {
-                // 在这里把时间修改为相对事件
-                result.forEach(function (element) {
-                    element.addtime = moment(element.addtime).format('YYYY-MM-DD HH:mm:ss');
-                })
-            }
-            let ret = [result[0], result[1], result[2], result[3], result[4]];
-            // 向上取整
-            let pageNum = Math.ceil(result.length / pageSize);
-             // console.log('----------------------------1111' ,ret)
-            return res.json({
-                code : 1,
-                comments: ret,
+            result.forEach(function (element) {
+                element.addtime = moment(element.addtime).startOf('second').fromNow();
+            })
+            // 开始渲染数据到前台
+            res.json({
+                code: 1,
+                comments: result,
                 pageNum : pageNum
             });
-        })
-        return;
-    }
-
-
-    // console.log(params)
-    // 开始去数据库中查询数据信息
-    Comment.getCommentByCurrentPage(params, function (err, result) {
-        if (err) {
-            return next(err);
-        }
-
-        // 修改时间显示的格式
-        result.forEach(function (element) {
-            element.addtime = moment(element.addtime).startOf('second').fromNow();
-        })
-
-        // console.log('------------------------------222',result)
-        // 开始渲染数据到前台
-        res.json({
-            user : req.session.user,
-            code: 1,
-            comments: result
         });
-    });
+    })
 
 }
 
